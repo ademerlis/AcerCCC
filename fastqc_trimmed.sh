a503f3b65fa5b14a4c9591bd443b0be1ea90a62d
Allyson DeMerlis <allyson.demerlis@gmail.com>
2023-06-03 15:07:37 -0400

    Update fastqc_trimmed.sh

--- fastqc_trimmed.sh
+++ fastqc_trimmed.sh
@@ -4,7 +4,7 @@
 #purpose: quality checking of trimmed reads using FASTQC on Pegasus compute node
 
 #BSUB -J AcerCCC_fastqc_trimmed
-#BSUB -q general
+#BSUB -q debug
 #BSUB -P and_transcriptomics
 #BSUB -o fastqc_trimmed.out
 #BSUB -e fastqc_trimmed.err
@@ -19,7 +19,6 @@ samples=${and}/Allyson_CCC/trimmed/samples.txt
 # trimming the files
 for SAMP in `cat ${samples}`
 do
-
 echo '#!/bin/bash' > /scratch/projects/and_transcriptomics/Allyson_CCC/trimmed/${SAMP}_trimming.job
 echo '#BSUB -q general' >> /scratch/projects/and_transcriptomics/Allyson_CCC/trimmed/${SAMP}_trimming.job
 echo '#BSUB -J '$SAMP'' >> /scratch/projects/and_transcriptomics/Allyson_CCC/trimmed/${SAMP}_trimming.job
@@ -30,7 +29,7 @@ echo 'module load fastqc/0.10.1' >> /scratch/projects/and_transcriptomics/Allyso
 
 echo 'cd ${and}/trimmed/' >> /scratch/projects/and_transcriptomics/Allyson_CCC/trimmed/${SAMP}_trimming.job
 
-echo 'fastqc *.fastq.gz
+echo 'fastqc *.fq.gz
 --outdir ${and}/trimmed/' >> /scratch/projects/and_transcriptomics/Allyson_CCC/trimmed/${SAMP}_trimming.job
 
 done
