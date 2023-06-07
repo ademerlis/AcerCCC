1141fd198c8f331b7627cae17bae75d1d83aa320
Allyson DeMerlis <allyson.demerlis@gmail.com>
2023-06-07 14:34:53 -0400

    editing scripts to match current ones on pegasus

--- fastqc_trimmed.sh
+++ fastqc_trimmed.sh
@@ -3,8 +3,8 @@
 #/scratch/projects/and_transcriptomics/Allyson_CCC/scripts/fastqc_trimmed.sh
 #purpose: quality checking of trimmed reads using FASTQC on Pegasus compute node
 
-#BSUB -J AcerCCC_fastqc_trimmed
-#BSUB -q debug
+#BSUB -J fastqc_trimmed
+#BSUB -q general
 #BSUB -P and_transcriptomics
 #BSUB -o fastqc_trimmed.out
 #BSUB -e fastqc_trimmed.err
@@ -14,23 +14,11 @@
 
 and="/scratch/projects/and_transcriptomics/Allyson_CCC"
 
-samples=${and}/Allyson_CCC/trimmed/samples.txt
+module load fastqc/0.10.1
 
-# trimming the files
-for SAMP in `cat ${samples}`
-do
-echo '#!/bin/bash' > /scratch/projects/and_transcriptomics/Allyson_CCC/trimmed/${SAMP}_trimming.job
-echo '#BSUB -q general' >> /scratch/projects/and_transcriptomics/Allyson_CCC/trimmed/${SAMP}_trimming.job
-echo '#BSUB -J '$SAMP'' >> /scratch/projects/and_transcriptomics/Allyson_CCC/trimmed/${SAMP}_trimming.job
-echo '#BSUB -o /scratch/projects/transcriptomics/allysondemerlis/scripts/'$SAMP'_error_trimming.txt' >> /scratch/projects/and_transcriptomics/Allyson_CCC/trimmed/${SAMP}_trimming.job
-echo '#BSUB -e /scratch/projects/transcriptomics/allysondemerlis/scripts/'$SAMP'_output_trimming.txt' >> /scratch/projects/and_transcriptomics/Allyson_CCC/trimmed/${SAMP}_trimming.job
+cd ${and}/trimmed
 
-echo 'module load fastqc/0.10.1' >> /scratch/projects/and_transcriptomics/Allyson_CCC/trimmed/${SAMP}_trimming.job
+fastqc *.fastq.gz
+--outdir ${and}/trimmed/
 
-echo 'cd ${and}/trimmed/' >> /scratch/projects/and_transcriptomics/Allyson_CCC/trimmed/${SAMP}_trimming.job
-
-echo 'fastqc *.fq.gz
---outdir ${and}/trimmed/' >> /scratch/projects/and_transcriptomics/Allyson_CCC/trimmed/${SAMP}_trimming.job
-
-done
 
--- trimming_trimgalore.sh
+++ trimming_trimgalore.sh
@@ -1,32 +1,42 @@
 #!/bin/bash
 #BSUB -J trim_CCC
-#BSUB -q parallel
-#BSUB -R "span[ptile=16]"
+#BSUB -q bigmem
 #BSUB -P and_transcriptomics
 #BSUB -n 16
 #BSUB -W 120:00
-#BSUB -o trim_CCC%J.out
-#BSUB -e trim_CCC%J.err
+#BSUB -o trim_CCC.out
+#BSUB -e trim_CCC.err
 #BSUB -u and128@miami.edu
 #BSUB -N
 
 and="/scratch/projects/and_transcriptomics"
 
-for sample in ${and}/Allyson_CCC/fastq_files/*.gz ;
+tworemainingsamples="2383"
+
+echo "These are the samples to be processed:"
+echo $tworemainingsamples
+
+for sample in ${and}/Allyson_CCC/fastq_rawreads/${tworemainingsamples[*]}.fastq.gz ;
 
 do \
-${and}/programs/TrimGalore-0.6.10/trim_galore ${sample} \
---gzip \
---fastqc \
---fastqc_args "--outdir ${and}/Allyson_CCC/trimmed/" \
+cd ${and}/Allyson_CCC/fastq_rawreads/
+${and}/programs/TrimGalore-0.6.10/trim_galore ${sample}
 --illumina \
 --cores 4 \
 --three_prime_clip_R1 12 \
 --nextseq 30 \
 --length 20 \
--o ${and}/Allyson_CCC/trimmed/ ; \
+--outdir ${and}/Allyson_CCC/trimmed/ ; \
 
 done
+
+cd ${and}/Allyson_CCC/trimmed/
+
+mv *.fq.gz *.fastq.gz
+
+module load fastqc/0.10.1
+
+fastqc *.fastq.gz
 
 multiqc ${and}/Allyson_CCC/trimmed/ \
 --outdir ${and}/Allyson_CCC/trimmed/
