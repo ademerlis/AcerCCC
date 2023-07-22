60ee722a560a703da26c53226e0ab2435b42bcb3
Allyson DeMerlis <allyson.demerlis@gmail.com>
2023-07-22 17:14:46 -0400

    update stringtie code

--- scripts/8_stringtie.sh
+++ scripts/8_stringtie.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#BSUB -J stringtie
+#BSUB -J stringtie_updatedannotations_take3
 #BSUB -q general
 #BSUB -P and_transcriptomics
-#BSUB -o stringtie%J.out
-#BSUB -e stringtie%J.err
+#BSUB -o stringtie_updatedannotations_take3%J.out
+#BSUB -e stringtie_updatedannotations_take3%J.err
 #BSUB -u and128@miami.edu
 #BSUB -N
 
@@ -11,12 +11,12 @@ module load python/3.8.7
 
 and="/scratch/projects/and_transcriptomics"
 
-cd "/scratch/projects/and_transcriptomics/Allyson_CCC/aligned"
+cd "/scratch/projects/and_transcriptomics/Ch4_AcerCCC/aligned_updatedannotations_take3"
 
 data=($(ls *Aligned.sortedByCoord.out.bam))
 
 for i in ${data[@]} ;
 
 do \
-/scratch/projects/and_transcriptomics/programs/stringtie-2.2.1/stringtie -G /scratch/projects/and_transcriptomics/genomes/Acer/Acerv_assembly_v1.0.gff3 -o ${i}.gtf ${i} ; \ 
+/scratch/projects/and_transcriptomics/programs/stringtie-2.2.1/stringtie -p 8 -e -B -G /scratch/projects/and_transcriptomics/genomes/Acer/Acerv.GFFannotations.fixed_transcript_take3.gff3 -A ${i}.gene_abund.tab -o ${i}.gtf ${i} ; \
 done
