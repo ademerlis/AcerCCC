f67c215eb0a3aee70d6e0c0c9220b0d30398edbf
Allyson DeMerlis <allyson.demerlis@gmail.com>
2024-03-15 11:48:26 -0400

    final bioinformatics steps

---
+++ bioinformatics/archive/1_fastqc_rawreads.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#~/scripts/fastqc_AcerCCC.sh
+#/scratch/projects/and_transcriptomics/Allyson_CCC/scripts/fastqc_AcerCCC.sh
+#purpose: quality checking of raw RNAseq reads using FASTQC on Pegasus compute node
+
+#BSUB -J AcerCCC_fastqc
+#BSUB -q general
+#BSUB -P and_transcriptomics
+#BSUB -o fastqc_AcerCCC.out
+#BSUB -e fastqc_AcerCCC.err
+#BSUB -n 8
+#BSUB -u allyson.demerlis@earth.miami.edu
+#BSUB -N
+
+module load fastqc/0.10.1
+
+and="/scratch/projects/and_transcriptomics/Allyson_CCC"
+
+cd ${and}
+fastqc *.fastq.gz
+--outdir ${and}/fastqc/
+
